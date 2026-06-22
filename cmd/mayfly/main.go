a223864d53fbb21ccb96fd487e0f8d9aabb75258
DWoodhouse22 <dwoodhouse22@gmail.com>
2026-06-22 10:04:47 +0100

    use cobra for server start command, placeholder test

--- cmd/mayfly/main.go
+++ cmd/mayfly/main.go
@@ -1,57 +1,5 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-
-	"mayfly/internal/keygen"
-	sshclient "mayfly/internal/ssh"
-)
-
 func main() {
-	host := flag.String("host", "", "VPS host to connect to (user@host)")
-	user := flag.String("user", "root", "SSH user")
-	key := flag.String("key", "", "SSH private key file (default: ~/.ssh/id_ed25519)")
-	port := flag.Int("port", 22, "SSH port")
-	flag.Parse()
-
-	if *host != "" {
-		testSSH(*host, *user, *key, *port)
-		return
-	}
-
-	server, err := keygen.GenerateKeyPair()
-	if err != nil {
-		log.Fatalf("generate server keypair: %v", err)
-	}
-
-	client, err := keygen.GenerateKeyPair()
-	if err != nil {
-		log.Fatalf("generate client keypair: %v", err)
-	}
-
-	fmt.Println("Server:")
-	fmt.Printf("private: %s\n", server.PrivateKey)
-	fmt.Printf("public:  %s\n", server.PublicKey)
-	fmt.Println("Client:")
-	fmt.Printf("private: %s\n", client.PrivateKey)
-	fmt.Printf("public:  %s\n", client.PublicKey)
-}
-
-func testSSH(host, user, key string, port int) {
-	fmt.Printf("Connecting to %s@%s:%d...\n", user, host, port)
-
-	client, err := sshclient.Connect(host, user, port, key)
-	if err != nil {
-		log.Fatalf("SSH connection failed: %v", err)
-	}
-	defer client.Close()
-
-	out, err := client.Run("uname -sr")
-	if err != nil {
-		log.Fatalf("command failed: %v", err)
-	}
-
-	fmt.Printf("Connected. Remote is running: %s\n", out)
+	Execute()
 }
--- cmd/mayfly/server.go
+++ cmd/mayfly/server.go
@@ -1,6 +1,22 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"log"
+	sshclient "mayfly/internal/ssh"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	flagHost  string
+	flagUser  string
+	flagKey   string
+	flagPort  int
+	flagImage string
+	flagToken string
+)
 
 var serverCmd = &cobra.Command{
 	Use: "server",
@@ -16,9 +32,45 @@ var serverStopCmd = &cobra.Command{
 }
 
 func runServerStart(cmd *cobra.Command, args []string) error {
+	if flagToken == "" {
+		flagToken = os.Getenv("MAYFLY_TOKEN")
+	}
+	if flagToken == "" {
+		return fmt.Errorf("--token is required (or set MAYFLY_TOKEN)")
+	}
+
+	fmt.Printf("Connecting to %s@%s:%d...\n", flagUser, flagHost, flagPort)
+
+	client, err := sshclient.Connect(flagHost, flagUser, flagPort, flagKey)
+	if err != nil {
+		log.Fatalf("SSH connection failed: %v", err)
+	}
+	defer client.Close()
+
+	out, err := client.Run("uname -sr")
+	if err != nil {
+		log.Fatalf("command failed: %v", err)
+	}
+
+	fmt.Printf("Connected. Remote is running: %s\n", out)
+
 	return nil
 }
 
 func runServerStop(cmd *cobra.Command, args []string) error {
 	return nil
+}
+
+func init() {
+	serverCmd.AddCommand(serverStartCmd)
+	serverCmd.AddCommand(serverStopCmd)
+
+	serverStartCmd.Flags().StringVar(&flagHost, "host", "", "Hostname of the VPS")
+	serverStartCmd.Flags().StringVarP(&flagUser, "user", "u", "", "SSH login user")
+	serverStartCmd.Flags().StringVarP(&flagKey, "key", "k", "", "Path to SSH private key file")
+	serverStartCmd.Flags().IntVarP(&flagPort, "port", "p", 22, "SSH port")
+	serverStartCmd.Flags().StringVarP(&flagImage, "image", "i", "ghcr.io/DWoodhouse22/mayfly-server:latest", "Server docker image")
+	serverStartCmd.Flags().StringVarP(&flagToken, "token", "t", "", "Access token")
+
+	serverStartCmd.MarkFlagRequired("host")
 }
