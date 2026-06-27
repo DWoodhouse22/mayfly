617dcdf7fdf7a181d335dacd1eb6e739d53fecbe
Dave Woodhouse <dwoodhouse22@gmail.com>
2026-06-27 15:54:07 +0100

    [wip untested] configure DNS for linux

--- internal/tunnel/route_unix.go
+++ internal/tunnel/route_unix.go
@@ -6,15 +6,19 @@ import (
 	"fmt"
 	"mayfly/internal/config"
 	"net"
+	"os"
+	"os/exec"
 
 	"github.com/vishvananda/netlink"
 	"golang.zx2c4.com/wireguard/tun"
 )
 
 var (
-	origVpsIp net.IP
-	origGw    net.IP
-	origLIdx  int
+	origVpsIp           net.IP
+	origGw              net.IP
+	origLIdx            int
+	origResolvConf      []byte
+	usedSystemdResolved bool
 )
 
 func setupRouting(tunDevice tun.Device, config *config.ClientConfig) error {
@@ -73,17 +77,75 @@ func setupRouting(tunDevice tun.Device, config *config.ClientConfig) error {
 		return err
 	}
 
+	usedSystemdResolved = isSystemdResolved()
+	if usedSystemdResolved {
+		if err := exec.Command("resolvectl", "dns", "mayfly0", config.DNS).Run(); err != nil {
+			return fmt.Errorf("setting DNS: %w", err)
+		}
+		if err := exec.Command("resolvectl", "domain", "mayfly0", "~.").Run(); err != nil {
+			restoreDNS() // atempt cleanup
+			return fmt.Errorf("setting DNS domain: %w", err)
+		}
+	} else {
+		// fallback to /etc/resolv.conf
+		const resolvPath = "/etc/resolv.conf"
+		original, err := os.ReadFile(resolvPath)
+		if err != nil {
+			return err
+		}
+		origResolvConf = original
+		info, err := os.Stat(resolvPath)
+		if err != nil {
+			return err
+		}
+		if err := writeResolvConf([]byte("nameserver "+config.DNS+"\n"), info.Mode()); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
+func isSystemdResolved() bool {
+	return exec.Command("systemctl", "is-active", "--quiet", "systemd-resolved").Run() == nil
+}
+
 func teardownRouting() error {
-	// if err := netlink.RouteDel(&netlink.Route{
-	// 	LinkIndex: origLIdx,
-	// 	Dst:       &net.IPNet{IP: origVpsIP, Mask: net.CIDRMask(32, 32)},
-	// 	Gw:        origGw,
-	// }); err != nil {
-	// 	return fmt.Errorf("removing VPS host route: %w", err)
-	// }
-	// return restoreDNS()
+	if err := netlink.RouteDel(&netlink.Route{
+		LinkIndex: origLIdx,
+		Dst:       &net.IPNet{IP: origVpsIp, Mask: net.CIDRMask(32, 32)},
+		Gw:        origGw,
+	}); err != nil {
+		return fmt.Errorf("removing VPS host route: %w", err)
+	}
+	return restoreDNS()
+}
+
+func restoreDNS() error {
+	if usedSystemdResolved {
+		return exec.Command("resolvectl", "revert", "mayfly0").Run()
+	}
+	return writeResolvConf(origResolvConf, 0644)
+}
+
+func writeResolvConf(content []byte, mode os.FileMode) error {
+	const resolvPath = "/etc/resolv.conf"
+	tmpFile, err := os.CreateTemp("/etc", "resolv.conf.*")
+	if err != nil {
+		return err
+	}
+	if _, err := tmpFile.Write(content); err != nil {
+		os.Remove(tmpFile.Name())
+		return err
+	}
+	tmpFile.Close()
+	if err := os.Chmod(tmpFile.Name(), mode); err != nil {
+		os.Remove(tmpFile.Name())
+		return err
+	}
+	if err := os.Rename(tmpFile.Name(), resolvPath); err != nil {
+		os.Remove(tmpFile.Name())
+		return err
+	}
 	return nil
 }
